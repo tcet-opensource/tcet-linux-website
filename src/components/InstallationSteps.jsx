014bab6b95e362991967e937f9902520cb0581a2
Shudhanshu Singh <shudhanshusingh051@gmail.com>
2023-05-03 10:39:54 +0530

    finalizing the code

--- src/components/InstallationSteps.jsx
+++ src/components/InstallationSteps.jsx
@@ -70,16 +70,60 @@ function InstallationSteps() {
     },
   ];
   return (
-    <div className="overflow-hidden">
-      <div className="bg-[#101828]">
-        <div className="p-12 w-full h-full flex flex-col xl:flex-row md:flex-row">
+    <div className="overflow-hidden bg-[#101828]">
+      <div className="">
+        <div className="p-12 w-full h-full flex flex-col-reverse xl:flex-row md:flex-row">
           <div className="w-full xl:w-1/2">
             <p className="text-[#6172F3] font-bold">INSTALLATION GUIDE</p>
             <p className="font-title text-[#FCFCFD] text-4xl pt-2 pb-2">
               TCET Linux Installation Steps
             </p>
+            <div className="absolute z-30 top-14 left-0 w-48 h-80  bg-gradient-to-r from-teal-100 via-indigo-500 to-pink-100 opacity-30 blur-2xl flex-none order-4 rounded-xl transform -rotate-45"></div>
             <div onMouseLeave={() => setImage(welcomeImage)}>
               {installationStepsFirstSet.map((step) => (
+                <div
+                  className="pt-6 cursor-pointer"
+                  onMouseEnter={() => setImage(step.image)}
+                  key={step.id}
+                >
+                  <p className="text-[#98A2B3] font-semibold">
+                    {step.id}. {step.header}
+                  </p>
+                  <p className="text-[#F9FAFB] pt-2">{step.description}</p>
+                </div>
+              ))}
+            </div>
+            <div className="mt-8 hidden xl:block md:block">
+              <button className="text-[#F9FAFB] border border-[#667085] rounded-lg p-3">
+                {" "}
+                Download
+              </button>
+            </div>
+          </div>
+          <div className="relative w-full flex justify-center align-middle xl:w-1/2">
+            <div className="absolute z-30 top-5 w-48 h-52 xl:w-[600px] xl:h-[450px] bg-[#98A2B3] opacity-30 blur-2xl flex-none order-4 rounded-full"></div>
+            <div className="z-50 flex align-middle rounded-xl m-2">
+              <img className="w-full object-contain rounded-lg" src={image} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="pl-12 pr-12 pb-12 xl:pt-12 md:pt-12 sm:pt-0 w-full h-full flex flex-col xl:flex-row md:flex-row">
+          <div className="relative w-full flex justify-center align-middle xl:w-1/2">
+            <div className="absolute z-30 top-5 w-48 h-52 xl:w-[600px] xl:h-[450px] bg-[#98A2B3] opacity-30 blur-2xl flex-none order-4 rounded-full"></div>
+            <div className="z-50 flex align-middle rounded-xl m-2">
+              <img className="w-full object-contain rounded-lg hidden xl:block md:block" src={image} />
+            </div>
+          </div>
+          <div className="relative w-full xl:w-1/2 sm:pl-0 md:pl-6 xl:pl-6">
+            <p className="text-[#6172F3] font-bold hidden xl:block md:block">INSTALLATION GUIDE</p>
+            <p className="font-title text-[#FCFCFD] text-4xl pt-2 pb-2 hidden xl:block md:block">
+              TCET Linux Installation Steps
+            </p>
+            <div className="absolute z-30 top-20 right-0 w-48 h-52 bg-gradient-to-r from-teal-100 via-indigo-500 to-pink-100 opacity-30 blur-2xl flex-none order-4 rounded-full transform rotate-45"></div>
+            <div onMouseLeave={() => setImage(welcomeImage)}>
+              {installationStepsSecondSet.map((step) => (
                 <div
                   className="pt-6 cursor-pointer"
                   onMouseEnter={() => setImage(step.image)}
@@ -99,12 +143,7 @@ function InstallationSteps() {
               </button>
             </div>
           </div>
-          <div className="relative w-full flex justify-center align-middle xl:w-1/2">
-            <div className="absolute z-30 top-5 w-48 h-52 xl:w-[600px] xl:h-[450px] bg-[#98A2B3] opacity-30 blur-lg flex-none order-4 rounded-full"></div>
-            <div className="z-50 flex align-middle rounded-xl m-2">
-              <img className="w-full object-contain rounded-lg" src={image} />
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
