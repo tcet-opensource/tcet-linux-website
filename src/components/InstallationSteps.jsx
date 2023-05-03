c64b3a6f6a764e6d664c18c9f10bf57b8b9ed14f
Shudhanshu Singh <shudhanshusingh051@gmail.com>
2023-05-04 00:10:59 +0530

    adding 2 screens & 2 states for both section

--- src/components/InstallationSteps.jsx
+++ src/components/InstallationSteps.jsx
@@ -8,7 +8,8 @@ import summaryImage from "../assets/image 14.png";
 import installationImage from "../assets/image 15.png";
 import finishImage from "../assets/image 16.png";
 function InstallationSteps() {
-  const [image, setImage] = useState(welcomeImage);
+  const [imageFirst, setImageFirst] = useState(welcomeImage);
+  const [imageSecond, setImageSecond] = useState(welcomeImage);
   const installationStepsFirstSet = [
     {
       id: 1,
@@ -79,11 +80,11 @@ function InstallationSteps() {
               TCET Linux Installation Steps
             </p>
             <div className="absolute z-30 top-14 left-0 w-48 h-80  bg-gradient-to-r from-teal-100 via-indigo-500 to-pink-100 opacity-30 blur-2xl flex-none order-4 rounded-xl transform -rotate-45"></div>
-            <div onMouseLeave={() => setImage(welcomeImage)}>
+            <div onMouseLeave={() => setImageFirst(welcomeImage)}>
               {installationStepsFirstSet.map((step) => (
                 <div
                   className="pt-6 cursor-pointer"
-                  onMouseEnter={() => setImage(step.image)}
+                  onMouseEnter={() => setImageFirst(step.image)}
                   key={step.id}
                 >
                   <p className="text-[#98A2B3] font-semibold">
@@ -103,7 +104,7 @@ function InstallationSteps() {
           <div className="relative w-full flex justify-center align-middle xl:w-1/2">
             <div className="absolute z-30 top-5 w-48 h-52 xl:w-[600px] xl:h-[450px] bg-[#98A2B3] opacity-30 blur-2xl flex-none order-4 rounded-full"></div>
             <div className="z-50 flex align-middle rounded-xl m-2">
-              <img className="w-full object-contain rounded-lg" src={image} />
+              <img className="w-full object-contain rounded-lg" src={imageFirst} />
             </div>
           </div>
         </div>
@@ -113,20 +114,20 @@ function InstallationSteps() {
           <div className="relative w-full flex justify-center align-middle xl:w-1/2">
             <div className="absolute z-30 top-5 w-48 h-52 xl:w-[600px] xl:h-[450px] bg-[#98A2B3] opacity-30 blur-2xl flex-none order-4 rounded-full"></div>
             <div className="z-50 flex align-middle rounded-xl m-2">
-              <img className="w-full object-contain rounded-lg hidden xl:block md:block" src={image} />
+              <img className="w-full object-contain rounded-lg" src={imageSecond} />
             </div>
           </div>
           <div className="relative w-full xl:w-1/2 sm:pl-0 md:pl-6 xl:pl-6">
-            <p className="text-[#6172F3] font-bold hidden xl:block md:block">INSTALLATION GUIDE</p>
-            <p className="font-title text-[#FCFCFD] text-4xl pt-2 pb-2 hidden xl:block md:block">
+            <p className="text-[#6172F3] font-bold">INSTALLATION GUIDE</p>
+            <p className="font-title text-[#FCFCFD] text-4xl pt-2 pb-2">
               TCET Linux Installation Steps
             </p>
             <div className="absolute z-30 top-20 right-0 w-48 h-52 bg-gradient-to-r from-teal-100 via-indigo-500 to-pink-100 opacity-30 blur-2xl flex-none order-4 rounded-full transform rotate-45"></div>
-            <div onMouseLeave={() => setImage(welcomeImage)}>
+            <div onMouseLeave={() => setImageSecond(welcomeImage)}>
               {installationStepsSecondSet.map((step) => (
                 <div
                   className="pt-6 cursor-pointer"
-                  onMouseEnter={() => setImage(step.image)}
+                  onMouseEnter={() => setImageSecond(step.image)}
                   key={step.id}
                 >
                   <p className="text-[#98A2B3] font-semibold">
