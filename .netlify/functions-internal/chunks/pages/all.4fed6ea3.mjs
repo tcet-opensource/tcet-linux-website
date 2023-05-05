9cee4acf3fc1135a221dd5477ab7b64bc9ecad64
VedantPol <polvedant7@gmail.com>
2023-05-05 23:22:24 +0530

    added productino.yml for ci/cd pipeline

---
+++ .netlify/functions-internal/chunks/pages/all.4fed6ea3.mjs
@@ -0,0 +1,186 @@
+/* empty css                           */import { c as createAstro, a as createComponent, r as renderTemplate, b as addAttribute, s as spreadAttributes, u as unescapeHTML, d as renderComponent, e as renderHead, f as renderSlot, m as maybeRenderHead } from '../astro.28d78b0e.mjs';
+
+const $$Astro$8 = createAstro("https://linux.tcetmumbai.in");
+const $$OpenGraphArticleTags = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$8, $$props, $$slots);
+  Astro2.self = $$OpenGraphArticleTags;
+  const { publishedTime, modifiedTime, expirationTime, authors, section, tags } = Astro2.props.openGraph.article;
+  return renderTemplate`${publishedTime ? renderTemplate`<meta property="article:published_time"${addAttribute(publishedTime, "content")}>` : null}
+${modifiedTime ? renderTemplate`<meta property="article:modified_time"${addAttribute(modifiedTime, "content")}>` : null}
+${expirationTime ? renderTemplate`<meta property="article:expiration_time"${addAttribute(expirationTime, "content")}>` : null}
+${authors ? authors.map((author) => renderTemplate`<meta property="article:author"${addAttribute(author, "content")}>`) : null}
+${section ? renderTemplate`<meta property="article:section"${addAttribute(section, "content")}>` : null}
+${tags ? tags.map((tag) => renderTemplate`<meta property="article:tag"${addAttribute(tag, "content")}>`) : null}`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/components/OpenGraphArticleTags.astro");
+
+const $$Astro$7 = createAstro("https://linux.tcetmumbai.in");
+const $$OpenGraphBasicTags = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$7, $$props, $$slots);
+  Astro2.self = $$OpenGraphBasicTags;
+  const { openGraph } = Astro2.props;
+  return renderTemplate`<meta property="og:title"${addAttribute(openGraph.basic.title, "content")}>
+<meta property="og:type"${addAttribute(openGraph.basic.type, "content")}>
+<meta property="og:image"${addAttribute(openGraph.basic.image, "content")}>
+<meta property="og:url"${addAttribute(openGraph.basic.url || Astro2.url.href, "content")}>`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/components/OpenGraphBasicTags.astro");
+
+const $$Astro$6 = createAstro("https://linux.tcetmumbai.in");
+const $$OpenGraphImageTags = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$6, $$props, $$slots);
+  Astro2.self = $$OpenGraphImageTags;
+  const { image } = Astro2.props.openGraph.basic;
+  const { secureUrl, type, width, height, alt } = Astro2.props.openGraph.image;
+  return renderTemplate`<meta property="og:image:url"${addAttribute(image, "content")}>
+${secureUrl ? renderTemplate`<meta property="og:image:secure_url"${addAttribute(secureUrl, "content")}>` : null}
+${type ? renderTemplate`<meta property="og:image:type"${addAttribute(type, "content")}>` : null}
+${width ? renderTemplate`<meta property="og:image:width"${addAttribute(width, "content")}>` : null}
+${!(height === null) ? renderTemplate`<meta property="og:image:height"${addAttribute(height, "content")}>` : null}
+${!(alt === null) ? renderTemplate`<meta property="og:image:alt"${addAttribute(alt, "content")}>` : null}`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/components/OpenGraphImageTags.astro");
+
+const $$Astro$5 = createAstro("https://linux.tcetmumbai.in");
+const $$OpenGraphOptionalTags = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$5, $$props, $$slots);
+  Astro2.self = $$OpenGraphOptionalTags;
+  const { optional } = Astro2.props.openGraph;
+  return renderTemplate`${optional.audio ? renderTemplate`<meta property="og:audio"${addAttribute(optional.audio, "content")}>` : null}
+${optional.description ? renderTemplate`<meta property="og:description"${addAttribute(optional.description, "content")}>` : null}
+${optional.determiner ? renderTemplate`<meta property="og:determiner"${addAttribute(optional.determiner, "content")}>` : null}
+${optional.locale ? renderTemplate`<meta property="og:locale"${addAttribute(optional.locale, "content")}>` : null}
+${optional.localeAlternate?.map((locale) => renderTemplate`<meta property="og:locale:alternate"${addAttribute(locale, "content")}>`)}
+${optional.siteName ? renderTemplate`<meta property="og:site_name"${addAttribute(optional.siteName, "content")}>` : null}
+${optional.video ? renderTemplate`<meta property="og:video"${addAttribute(optional.video, "content")}>` : null}`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/components/OpenGraphOptionalTags.astro");
+
+const $$Astro$4 = createAstro("https://linux.tcetmumbai.in");
+const $$ExtendedTags = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$4, $$props, $$slots);
+  Astro2.self = $$ExtendedTags;
+  const { props } = Astro2;
+  return renderTemplate`${props.extend.link?.map((attributes) => renderTemplate`<link${spreadAttributes(attributes)}>`)}
+${props.extend.meta?.map(({ content, httpEquiv, name, property }) => renderTemplate`<meta${addAttribute(content, "content")}${addAttribute(httpEquiv, "http-equiv")}${addAttribute(name, "name")}${addAttribute(property, "property")}>`)}`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/components/ExtendedTags.astro");
+
+const $$Astro$3 = createAstro("https://linux.tcetmumbai.in");
+const $$TwitterTags = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$3, $$props, $$slots);
+  Astro2.self = $$TwitterTags;
+  const { card, site, title, creator, description, image, imageAlt } = Astro2.props.twitter;
+  return renderTemplate`${card ? renderTemplate`<meta name="twitter:card"${addAttribute(card, "content")}>` : null}
+${site ? renderTemplate`<meta name="twitter:site"${addAttribute(site, "content")}>` : null}
+${title ? renderTemplate`<meta name="twitter:title"${addAttribute(title, "content")}>` : null}
+${image ? renderTemplate`<meta name="twitter:image"${addAttribute(image, "content")}>` : null}
+${imageAlt ? renderTemplate`<meta name="twitter:image:alt"${addAttribute(imageAlt, "content")}>` : null}
+${description ? renderTemplate`<meta name="twitter:description"${addAttribute(description, "content")}>` : null}
+${creator ? renderTemplate`<meta name="twitter:creator"${addAttribute(creator, "content")}>` : null}`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/components/TwitterTags.astro");
+
+const $$Astro$2 = createAstro("https://linux.tcetmumbai.in");
+const $$SEO = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  Astro2.self = $$SEO;
+  Astro2.props.surpressWarnings = true;
+  function validateProps(props) {
+    if (props.openGraph) {
+      if (!props.openGraph.basic || props.openGraph.basic.title == null || props.openGraph.basic.type == null || props.openGraph.basic.image == null) {
+        throw new Error(
+          "If you pass the openGraph prop, you have to at least define the title, type, and image basic properties!"
+        );
+      }
+    }
+    if (props.title && props.openGraph?.basic.title) {
+      if (props.title == props.openGraph.basic.title && !props.surpressWarnings) {
+        console.warn(
+          "WARNING(astro-seo): You passed the same value to `title` and `openGraph.optional.title`. This is most likely not what you want. See docs for more."
+        );
+      }
+    }
+    if (props.openGraph?.basic?.image && !props.openGraph?.image?.alt && !props.surpressWarnings) {
+      console.warn(
+        "WARNING(astro-seo): You defined `openGraph.basic.image`, but didn't define `openGraph.image.alt`. This is stongly discouraged.'"
+      );
+    }
+  }
+  validateProps(Astro2.props);
+  let updatedTitle = "";
+  if (Astro2.props.title) {
+    updatedTitle = Astro2.props.title;
+    if (Astro2.props.titleTemplate) {
+      updatedTitle = Astro2.props.titleTemplate.replace(/%s/g, updatedTitle);
+    }
+  } else if (Astro2.props.titleDefault) {
+    updatedTitle = Astro2.props.titleDefault;
+  }
+  return renderTemplate`${updatedTitle ? renderTemplate`<title>${unescapeHTML(updatedTitle)}</title>` : null}
+
+${Astro2.props.charset ? renderTemplate`<meta${addAttribute(Astro2.props.charset, "charset")}>` : null}
+
+<link rel="canonical"${addAttribute(Astro2.props.canonical || Astro2.url.href, "href")}>
+
+${Astro2.props.description ? renderTemplate`<meta name="description"${addAttribute(Astro2.props.description, "content")}>` : null}
+
+<meta name="robots"${addAttribute(`${Astro2.props.noindex ? "noindex" : "index"}, ${Astro2.props.nofollow ? "nofollow" : "follow"}`, "content")}>
+
+${Astro2.props.openGraph && renderTemplate`${renderComponent($$result, "OpenGraphBasicTags", $$OpenGraphBasicTags, { ...Astro2.props })}`}
+${Astro2.props.openGraph?.optional && renderTemplate`${renderComponent($$result, "OpenGraphOptionalTags", $$OpenGraphOptionalTags, { ...Astro2.props })}`}
+${Astro2.props.openGraph?.image && renderTemplate`${renderComponent($$result, "OpenGraphImageTags", $$OpenGraphImageTags, { ...Astro2.props })}`}
+${Astro2.props.openGraph?.article && renderTemplate`${renderComponent($$result, "OpenGraphArticleTags", $$OpenGraphArticleTags, { ...Astro2.props })}`}
+${Astro2.props.twitter && renderTemplate`${renderComponent($$result, "TwitterTags", $$TwitterTags, { ...Astro2.props })}`}
+${Astro2.props.extend && renderTemplate`${renderComponent($$result, "ExtendedTags", $$ExtendedTags, { ...Astro2.props })}`}`;
+}, "/home/albedo/github/tcet-linux-website/node_modules/astro-seo/src/SEO.astro");
+
+const $$Astro$1 = createAstro("https://linux.tcetmumbai.in");
+const $$Layout = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$Layout;
+  const { title, description } = Astro2.props;
+  return renderTemplate`<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Sora:wght@600&display=swap" rel="stylesheet">
+    ${renderComponent($$result, "SEO", $$SEO, { "title": title, "titleDefault": "TCET Linux | Home ", "description": description, "openGraph": {
+    basic: {
+      title,
+      type: "website",
+      image: "https://tcet.ac.in/images/logo.png"
+    }
+  } })}
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#ffffff">
+  ${renderHead($$result)}</head>
+  <body class="font-sans">
+    ${renderSlot($$result, $$slots["default"])}
+  </body></html>`;
+}, "/home/albedo/github/tcet-linux-website/src/layouts/Layout.astro");
+
+const $$Astro = createAstro("https://linux.tcetmumbai.in");
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "TCET Linux", "description": "Linux for TCET Services" }, { "default": ($$result2) => renderTemplate`
+	${maybeRenderHead($$result2)}<main class="text-lg">
+		yolo your parents retirement funds on futures and options stocks
+	</main>
+` })}`;
+}, "/home/albedo/github/tcet-linux-website/src/pages/index.astro");
+
+const $$file = "/home/albedo/github/tcet-linux-website/src/pages/index.astro";
+const $$url = "";
+
+const _page0 = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+  __proto__: null,
+  default: $$Index,
+  file: $$file,
+  url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { _page0 as _ };
