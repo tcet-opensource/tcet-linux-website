2116446400512c65df3ab2b512b5feded5f24d7b
VedantPol <polvedant7@gmail.com>
2023-05-05 18:58:56 +0530

    added astro dependencies for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap(), robotsTxt()],
-  site: "https://linux.tcetmumbai.in"
+  site: "https://linux.tcetmumbai.in",
+  output: "server",
+  adapter: netlify()
 });
