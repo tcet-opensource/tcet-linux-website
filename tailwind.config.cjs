ae51f100b8f57db78a1887975a351a83871effb9
Rai-Aaditya <adityaanchalarai@gmail.com>
2023-05-06 16:16:51 +0530

    [FIX]: font and button styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,12 @@ module.exports = {
 				title: ["Sora", ...defaultTheme.fontFamily.serif],
 			},
 			backgroundImage: {
-				'custom-bg': "url(images/download-background.png)"
+				'custom-bg': "url(images/download-background.png)",
+			},
+			backgroundColor:
+			{
+				'custom-btn-blue': "#01105F",
+				'custom-btn': "#2D3282",
 			},
 		},
 	},
